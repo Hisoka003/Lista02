20f1f468822ecc80aefef78db10c69d83c7454fc
Hisoka003 <148825311+Hisoka003@users.noreply.github.com>
2023-12-06 14:19:47 -0300

    Add files via upload

---
+++ ConsoleApp8/Exercicio01.cs
@@ -0,0 +1,28 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace ConsoleApp8
+{
+    public class Exercicio01
+    {
+        public static void Executar()
+        {
+            Console.WriteLine("CALCULO MÉDIA DE ESTOQUE MINIMO E MAXIMO");
+            Console.WriteLine("insira o NOME do prodtuto");
+            string Nome = Console.ReadLine();
+            Console.WriteLine("insira o estoque minimo do prodtuto");
+            int minimo = int.Parse(Console.ReadLine());
+            Console.WriteLine("insira o estoque MAXIMO do prodtuto");
+            int maximo = int.Parse(Console.ReadLine());
+
+            int media = minimo + maximo / 2;
+
+            Console.WriteLine("a media do produto " + Nome + "é:" + media);
+
+            Console.ReadKey();
+        }
+    }
+}
